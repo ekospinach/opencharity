ebf4357caae90c29657129418d719cf27d41d905
google.account <google.account@rain.com.au>
2008-03-31 22:57:46 +0000

    Import Cake Framework

---
+++ app/config/database.php
@@ -0,0 +1,12 @@
+<?php
+class DATABASE_CONFIG
+{
+  var $default = array('driver'   => 'mysql',
+                       'connect'  => 'mysql_connect',
+                       'host'     => 'localhost',
+                       'login'    => 'rai3761_openchar',
+                       'password' => 'rolgen45',
+                       'database' => 'rai3761_opencharity',
+                       'prefix'   => '');
+}
+
